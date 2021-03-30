a80ce3bfe723a287fc12571534aa5d86b587b582
Kim Kahre <kim.kahre@helsinki.fi>
2021-03-30 22:21:03 +0300

    coverage

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,14 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_on_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_lataaminen_kasvattaa_oikein(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(self.maksukortti.saldo, 20)
+
+    def test_lataaminen_kasvattaa_oikein(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
+        self.assertEqual(self.maksukortti.ota_rahaa(10), False)
